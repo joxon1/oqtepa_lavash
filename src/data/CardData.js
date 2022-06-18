a1b9b90e78c7e2c1a29623f4a95f7ca6d43dc096
rakhimjonovjoxongir@gmail.com <jahongir.r1999@gmail.com>
2022-06-18 16:34:15 +0500

    card data

--- src/data/CardData.js
+++ src/data/CardData.js
@@ -11,7 +11,33 @@ import kamtaronaSet from "../assets/set_img/kamtaronaSet.jfif";
 import muxabbatSeti from "../assets/set_img/muxabbatSet.jfif";
 import dostlarSeti from "../assets/set_img/dostlarSeti.jfif";
 
-import xaggi from "../assets/xaggi_img/xaggi.jfif"
+import xaggi from "../assets/xaggi_img/xaggi.jfif";
+
+import olchaliTort from "../assets/sweets_img/olchaliTort.jfif";
+import chizkeyk from "../assets/sweets_img/chizkeyk.jfif";
+import asalliTort from "../assets/sweets_img/asalliTort.jfif";
+
+import assorti from "../assets/pizza_img/assorti.jfif";
+import goshtli from "../assets/pizza_img/goshtli.jfif";
+import qazili from "../assets/pizza_img/qazili.jfif";
+import peperoni from "../assets/pizza_img/peperoni.jfif";
+import tovuqli from "../assets/pizza_img/tovuqli.jfif";
+import qoziqorin from "../assets/pizza_img/qoziqorin.jfif";
+
+import klabSendvich from "../assets/klab_img/slabSendvich.jfif";
+
+import gamburger from "../assets/burger_img/gamburger.jfif";
+import chizburger from "../assets/burger_img/chizburger.jfif";
+import bigBurger from "../assets/burger_img/bigburger.jfif";
+import bigChizburger from "../assets/burger_img/bigChiz.jfif";
+import shaurma from "../assets/burger_img/shaurma.jfif";
+import bigDoner from "../assets/burger_img/bigDoner.jfif";
+
+import xalapenyoHotdog from "../assets/hotdog_img/xalapenyoHotdog.jfif"
+import pishloqliHotdog from "../assets/hotdog_img/pishloqliHotdog.jfif"
+import hotdog from "../assets/hotdog_img/hotdog.jfif"
+import shoxonaHotdog from "../assets/hotdog_img/shohonaHotdog.jfif"
+
 
 export const Lavash = [
   {
@@ -57,6 +83,7 @@ export const Lavash = [
     price: 27000,
   },
 ];
+
 export const Set = [
   {
     title: "Klassik seti",
@@ -97,8 +124,140 @@ export const Xaggi = [
     image: xaggi,
     price: 26000,
   },
-]
+];
 
 export const Shirinlik = [
-  
-]
+  {
+    title: "Olchali tort",
+    desc: "Black Cherry",
+    image: olchaliTort,
+    price: 14000,
+  },
+  {
+    title: "Chizkeyk",
+    desc: "Black Cherry",
+    image: chizkeyk,
+    price: 14000,
+  },
+  {
+    title: "Asalli tort",
+    desc: "Black Cherry",
+    image: asalliTort,
+    price: 14000,
+  },
+];
+
+export const Pitsa = [
+  {
+    title: "Assorti pitsa",
+    desc: "Белый соус, оливки, шампиньоны, болгарский перец, помидоры, копченная индейка, копченная колбаса, говядина, сосиски, сыр Моцарелла и Акбел.",
+    image: assorti,
+    price: 74000,
+  },
+  {
+    title: "Go'shtli pitsa",
+    desc: "Томатный соус “OQTEPA”, куриное мясо, болгарский перец, говядина, помидоры, сыр Моцарелла и Акбел",
+    image: goshtli,
+    price: 75000,
+  },
+  {
+    title: "Qazili pitsa",
+    desc: "Томатный соус “OQTEPA”, кольца сладкого лука “Брунсвик”, казы, сыр Моцарелла и Акбел",
+    image: qazili,
+    price: 76000,
+  },
+  {
+    title: "Pepperoni pitsa",
+    desc: "Томатный соус “OQTEPA”, копченная колбаса, сыр Моцарелла и Акбел.",
+    image: peperoni,
+    price: 65000,
+  },
+  {
+    title: "Qo'ziqorinli pitsa",
+    desc: "Томатный соус OQ TEPA, грибы, чесночное масло, орегано, крупная паприка.",
+    image: qoziqorin,
+    price: 64000,
+  },
+  {
+    title: "Tovuqli pitsa",
+    desc: "Томатный соус OQ TEPA, индейка, курица, грибы, оливки, сыр, орегано",
+    image: tovuqli,
+    price: 65000,
+  },
+];
+
+export const KlabSendvich = [
+  {
+    title: "Klab Sendvich",
+    desc: "Тостерный хлеб, фирменный соус, огурцы, помидоры, куриное филе, латук, сыр, картофель фри",
+    image: klabSendvich,
+    price: 24000,
+  },
+];
+
+export const Burger = [
+  {
+    title: "Gamburger",
+    desc: "Сдобная булочка, фирменный соус, айсберг, солёные огурцы, говяжья котлета, помидоры, кольца сладкого лука «Брунсвик»",
+    image: gamburger,
+    price: 19000,
+  },
+  {
+    title: "Chizburger",
+    desc: "Сдобная булочка, фирменный соус, айсберг, солёные огурцы, говяжья котлета, помидоры, сыр, кольца сладкого лука «Брунсвик»",
+    image: chizburger,
+    price: 21000,
+  },
+  {
+    title: "Big burger",
+    desc: "Сдобная булочка, фирменный соус, айсберг, солёные огурцы, две говяжьи котлеты, помидоры, кольца сладкого лука «Брунсвик»",
+    image: bigBurger,
+    price: 27000,
+  },
+  {
+    title: "Big chizburger",
+    desc: "Сдобная булочка, фирменный соус, айсберг, солёные огурцы, две говяжьи котлеты, помидоры, сыр, кольца сладкого лука «Брунсвик»",
+    image: bigChizburger,
+    price: 32000,
+  },
+  {
+    title: "Shaurma",
+    desc: "Хлеб из тандыра «Пита», говядина, огурцы, помидоры, красный соус, кольца сладкого лука «Брунсвик»",
+    image: shaurma,
+    price: 19000,
+  },
+  {
+    title: "Big doner",
+    desc: "Авторский хлеб «Экмек», белый и красный соусы, чипсы, говядина, огурцы, помидоры.",
+    image: bigDoner,
+    price: 23000,
+  },
+];
+
+export const Hotdog = [
+  {
+    title: "Xalapenyoli hotdog",
+    desc: "Хот-дог булочка, фирменный соус, солёные огурцы, помидоры, халапеньо, сырный соус, латук, сосиска",
+    image: xalapenyoHotdog,
+    price: 13000,
+  },
+  {
+    title: "Pishloqli hotdog",
+    desc: "Хот-дог булочка, фирменный соус, солёные огурцы, помидоры, сырный соус, латук, сосиска",
+    image: pishloqliHotdog,
+    price: 11000,
+  },
+  {
+    title: "Hotdog",
+    desc: "Пышная булочка, сосиска, кетчуп, майонез, икра, огурцы, помидоры",
+    image: hotdog,
+    price: 10000,
+  },
+  {
+    title: "Shohona hotdog",
+    desc: "Хот-дог булочка, фирменный соус, солёные огурцы, помидоры, сырный соус, сыр, латук, две сосиски",
+    image: shoxonaHotdog,
+    price: 17000,
+  },
+];
+
