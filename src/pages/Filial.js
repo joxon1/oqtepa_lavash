87c5174554498371f75a0654458a1601b8df0221
rakhimjonovjoxongir@gmail.com <jahongir.r1999@gmail.com>
2022-06-23 01:19:16 +0500

    filial: full

--- src/pages/Filial.js
+++ src/pages/Filial.js
@@ -23,6 +23,12 @@ const Filial = () => {
           </div>
         ))}
       </div>
+      <div className="filial_maps">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d114192.06868509413!2d69.22249798199763!3d41.22742167645831!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae611b83047607%3A0xea68c1e94b4feb8e!2sOqtepa%20lavash!5e0!3m2!1sru!2s!4v1655928669693!5m2!1sru!2s"
+          loading="lazy"
+        ></iframe>
+      </div>
     </div>
   );
 };
